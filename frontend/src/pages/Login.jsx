33d3454e12c188e304549db937c3176691b53a21
Georgi Ovcharov <georgi.ovcharov12@gmail.com>
2022-06-20 16:06:53 +0300

    hook register & login to redux

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { FaSignInAlt } from "react-icons/fa";
-import { toast } from "react-toastify"
+import { toast } from "react-toastify";
+import { useSelector, useDispatch } from "react-redux";
+import { login } from "../features/auth/authSlice";
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -10,6 +12,12 @@ function Login() {
 
   const { email, password } = formData;
 
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
@@ -17,10 +25,15 @@ function Login() {
     }));
   };
 
-
   const onSubmit = (e) => {
-    e.preventDefault()
-  }
+    e.preventDefault();
+    const userData = {
+      email,
+      password,
+    };
+
+    dispatch(login(userData));
+  };
 
   return (
     <>
@@ -57,9 +70,7 @@ function Login() {
             />
           </div>
           <div className="form-group">
-            <button className="btn btn-block">
-              Submit
-            </button>
+            <button className="btn btn-block">Submit</button>
           </div>
         </form>
       </section>
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { FaUser } from "react-icons/fa";
-import { toast } from "react-toastify"
+import { toast } from "react-toastify";
+import { useSelector, useDispatch } from "react-redux";
+import { register } from "../features/auth/authSlice";
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -12,6 +14,12 @@ function Register() {
 
   const { name, email, password, password2 } = formData;
 
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
@@ -19,15 +27,20 @@ function Register() {
     }));
   };
 
-
   const onSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    if(password !== password2) {
-      toast.error("Passwords do not match!")
+    if (password !== password2) {
+      toast.error("Passwords do not match!");
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+      };
+      dispatch(register(userData));
     }
-
-  }
+  };
 
   return (
     <>
@@ -88,9 +101,7 @@ function Register() {
             />
           </div>
           <div className="form-group">
-            <button className="btn btn-block">
-              Submit
-            </button>
+            <button className="btn btn-block">Submit</button>
           </div>
         </form>
       </section>
