890bc45d85aee1b03d39a68e9a734250d81fd41b
Georgi Ovcharov <georgi.ovcharov12@gmail.com>
2022-06-21 10:49:06 +0300

    login & logout

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,17 +1,29 @@
-import axios from "axios"
+import axios from "axios";
 
-const API_URL = "/api/users"
+const API_URL = "/api/users/";
 
 const register = async (userData) => {
-  const response = await axios.post(API_URL, userData)
+  const response = await axios.post(API_URL, userData);
   if (response.data) {
-    localStorage.setItem("user", JSON.stringify(response.data))
+    localStorage.setItem("user", JSON.stringify(response.data));
   }
-  return response.data
-}
+  return response.data;
+};
+
+const login = async (userData) => {
+  const response = await axios.post(API_URL + "login", userData);
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+  return response.data;
+};
+
+const logout = () => localStorage.removeItem("user");
 
 const authService = {
-  register
-}
+  register,
+  login,
+  logout,
+};
 
-export default authService
+export default authService;
