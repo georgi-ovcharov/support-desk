3b298b7e9e0801e662f9da57a4efdb6fef0dbe95
Georgi Ovcharov <georgi.ovcharov12@gmail.com>
2022-06-23 16:38:57 +0300

    heroku preparation

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,32 @@
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
-const PORT = process.env.PORT || 8000
-const { errorHandler } = require ("./middleware/errorMiddleware")
-const connectDB = require("./config/db")
+const PORT = process.env.PORT || 8000;
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const path = require("path");
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/api/users", (req, res) => {
-  res.status(200).json({message: "Welcome"})
-})
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/tickets", require("./routes/ticketRoutes"));
 
-app.use("/api/users", require("./routes/userRoutes"))
-app.use("/api/tickets", require("./routes/ticketRoutes"))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
+  );
+} else {
+  app.get("/api/users", (req, res) => {
+    res.status(200).json({ message: "Welcome" });
+  });
+}
 
+app.use(errorHandler);
 
-app.use(errorHandler)
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
